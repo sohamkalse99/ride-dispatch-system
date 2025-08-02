8189c77dfe3ed99780718633e3f37515d9cde832
Soham Chandrashekhar Kalse <skalse@dons.usfca.edu>
2025-08-01 20:07:25 -0600

    added api_router import

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import router
 from fastapi.staticfiles import StaticFiles
+from app.api.api_router import api_router
 # Create FastAPI application
 app = FastAPI(
     title="Ride Dispatch System",
