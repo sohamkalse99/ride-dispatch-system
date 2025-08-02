156ab111c1592c934ca1828bfcfc192c2ae6af2b
Soham Chandrashekhar Kalse <skalse@dons.usfca.edu>
2025-08-01 20:10:47 -0600

    updated import

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import router
 from fastapi.staticfiles import StaticFiles
-from app.api.api_router import api_router
+from app.api.endpoints import router as api_router
 # Create FastAPI application
 app = FastAPI(
     title="Ride Dispatch System",
